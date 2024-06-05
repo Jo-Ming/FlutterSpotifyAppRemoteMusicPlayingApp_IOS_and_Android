47b9bff3d3507945b5b347059ccbecacc331466a
Joseph Rogers <jomingrogers@gmail.com>
2024-06-05 17:33:34 +0100

    dark theme update + colours

---
+++ lib/colours.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColours {
+  static const Color primaryColor = Color(0xFF00BCD4); // Cyan Blue
+  static const Color primaryVariant = Color(0xFF0097A7);
+  static const Color secondaryColor = Color(0xFF8BC34A); // Light Green
+  static const Color secondaryVariant = Color(0xFF558B2F);
+  static const Color backgroundColor = Color(0xFF303030); // Dark Grey
+  static const Color surfaceColor = Color(0xFF424242); // Slightly lighter grey
+  static const Color errorColor = Color(0xFFD32F2F); // Red
+  static const Color onPrimaryColor = Color(0xFFFFFFFF); // White
+  static const Color onSecondaryColor = Color(0xFF000000); // Black
+  static const Color onBackgroundColor = Color(0xFFFFFFFF); // White
+  static const Color onSurfaceColor = Color(0xFFFFFFFF); // White
+  static const Color onErrorColor = Color(0xFFFFFFFF); // White
+  static const Color buttonColor = Color(0xFF000000); // Black
+}
